344970fc281845ecc8f4b27d67a6d73fc4251180
mjcalderon <m.calderon@ext.redcapital.cl>
2025-07-16 20:22:59 -0400

    Se soluciona error model detalle

--- app/models/models.py
+++ app/models/models.py
@@ -102,9 +102,9 @@ class Detalle(Base):
     peso_recepcion = Column(Numeric(10, 2), nullable=True)
     cantidad = Column(Integer, nullable=False)
     id_guia = Column(String(50), nullable=False)
-    ruta_storage = Column(Text, nullable=True)
     tipo_de_diferencia = Column(String(20), nullable=False) 
     estado = Column(Boolean, default=True)
+
 # Tabla REPORTE
 class Reporte(Base):
     __tablename__ = "reporte"
